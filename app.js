44579061528793bfaa4d0af070df8c6acae28a80
Aman1601k <69238926+Aman1601k@users.noreply.github.com>
2021-08-04 13:21:25 +0530

    final commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+var PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const {MONGOURI} = require('./keys')
 const cors = require('cors')
